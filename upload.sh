530a6918d471221eb15b3aaa8ca30c0a1e3934c0
denysaja <143594138+denysaja@users.noreply.github.com>
2024-12-17 13:04:52 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/sagit/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/sagit/qassa*.zip sagit2:qassa -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X00TD/Aosp*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls Aosp*.zip)"
+      rclone copy ~/rom/out/target/product/X00TD/Aosp*.zip sagit2:qassa -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/X00TD/ && ls Aosp*.zip) Uploaded Successfully!"
 fi
